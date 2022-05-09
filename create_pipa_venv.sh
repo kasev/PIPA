8d230884c3ce4cefdf15a6a77b9145503bc0a4bb
kasev <vojtech.kase@gmail.com>
2022-05-09 08:53:31 +0200

    preparing venv, requirements etc.

--- create_pipa_venv.sh
+++ create_pipa_venv.sh
@@ -1,17 +1,6 @@
-#!/usr/bin/env bash
+virtualenv pipa_venv
 
-VENVNAME=pipa_venv
+pipa_venv/bin/python -m pip install --upgrade pip
+pipa_venv/bin/python -m pip install -r requirements.txt
 
-python3 -m venv $VENVNAME
-source $VENVNAME/bin/activate
-pip install --upgrade pip
-
-pip install ipython
-pip install jupyter
-
-python -m ipykernel install --user --name=$VENVNAME
-
-test -f requirements.txt && pip install -r requirements.txt
-
-deactivate
-echo "build $VENVNAME"
+pipa_venv/bin/python -m ipykernel install --user --name=pipa_venv
